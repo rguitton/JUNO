b88acf4377c8ae284bab90c82b085d4ff985c18f
roronoarapha <raphael.guitton@etu.unistra.fr>
2024-01-31 00:53:19 +0100

    modification puissance, lf to g, longueur

--- src/fonctions.cpp
+++ src/fonctions.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 double mass_neutron=939.565;//Mev
 double mass_proton=938.272;//Mev
-double mass_positron=0.510999;
+double mass_positron=0.510999;//Mev
 
-
+//pensez à remettre les coeffs 
 double flux_U_235(double E){
     double phi;
     phi=0.58*exp(0.870-0.160*E-0.091*pow(E,2));
@@ -15,7 +15,7 @@ double flux_U_235(double E){
 
 double flux_U_238(double E){
     double phi;
-    phi=0.30*exp(0.896-0.239*E-0.0981*pow(E,2));
+    phi=0.3*exp(0.896-0.239*E-0.0981*pow(E,2));
     return phi;
 }
 
@@ -40,7 +40,7 @@ double flux(double E){
 double sigma (double E){
     double energy_positron=E-(mass_neutron-mass_proton);
     double moment_positron=sqrt(pow(energy_positron,2)-pow(mass_positron,2));
-    return 0.0952*pow(10,-1)*energy_positron*moment_positron;
+    return 0.0952e-42*energy_positron*moment_positron;
 }
 
 double sin2_teta_12=0.32;
@@ -50,14 +50,14 @@ double teta_12=asin(sqrt(sin2_teta_12));
 double teta_13=asin(sqrt(sin2_2teta_13))/2;
 
 
-double delta2_m21=7.6*pow(10,-5);
-double delta2_m32=2.4*pow(10,-3);
+double delta2_m21=7.6e-5;
+double delta2_m32=2.4e-3;
 
 double delta2_m31_NH=delta2_m32+delta2_m21;
 double delta2_m31_IH=delta2_m32-delta2_m21;
 
 double probability(double E, char A, bool f){
-    double L=60*pow(10,3);
+    double L=53e3;//à changer
     double P21=pow(cos(teta_13),4)*pow(sin(2*teta_12),2)*pow(sin(1.27*delta2_m21*L/E),2);
     if(f==0){ return 1-P21;}
     else {
