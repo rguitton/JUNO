92aeaa8f60f4043a9d9060f2261e00eadcbed7a7
roronoarapha <raphael.guitton@etu.unistra.fr>
2024-02-01 08:31:57 +0100

    modification puissance, lf to g, longueur

--- include/fonctions.h
+++ include/fonctions.h
@@ -2,6 +2,11 @@
 #define FONCTIONS_H
 
 double flux(double E);
+double flux_U_235(double E);
+double flux_U_238(double E);
+double flux_PU_239(double E);
+double flux_PU_241(double E);
+
 double probability(double L, char A, bool f);
 double sigma (double E);
 
